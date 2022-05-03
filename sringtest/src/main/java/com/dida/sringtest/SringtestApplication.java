717216437dcfaeed43acfabe3b1d1c1fb96c7a76
HD0do； <zhanghd@neocrm.com>
2022-05-03 21:37:52 +0800

    修改注释

--- sringtest/src/main/java/com/dida/sringtest/SringtestApplication.java
+++ sringtest/src/main/java/com/dida/sringtest/SringtestApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SringtestApplication {
 
     public static void main(String[] args) {
+        //代码初始化入口
         SpringApplication.run(SringtestApplication.class, args);
     }
 
