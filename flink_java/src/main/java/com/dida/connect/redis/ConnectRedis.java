30eda4a20bcda84d6da625d4b72bf46434de1037
HD0do <zhanghd@neocrm.com>
2022-06-20 15:11:36 +0800

    修改

--- flink_java/src/main/java/com/dida/connect/redis/ConnectRedis.java
+++ flink_java/src/main/java/com/dida/connect/redis/ConnectRedis.java
@@ -9,15 +9,13 @@ import com.dida.utils.cdc.series;
 import com.ververica.cdc.connectors.mysql.source.MySqlSource;
 import com.ververica.cdc.connectors.mysql.table.StartupOptions;
 import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
-import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-import java.util.Map;
+import java.util.ArrayList;
 
 /**
  * @Author：zhd
@@ -27,28 +25,9 @@ import java.util.Map;
 public class ConnectRedis {
     public static void main(String[] args) throws Exception {
 
-        //获取传入参数
-        ParameterTool parameterTool = ParameterTool.fromArgs(args);
-        //环境变量获取
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment().setParallelism(1);
-
-        //获取配置
-        env.getConfig().setGlobalJobParameters(parameterTool);
-        ExecutionConfig.GlobalJobParameters globalJobParameters = env.getConfig().getGlobalJobParameters();
-        Map<String, String> map = globalJobParameters.toMap();
-
-        String mysqlHost = map.get("mysqlHost");
-        int mysqlPort =Integer.getInteger(map.get("mysqlPort")) ;
-        String mysqlPassword = map.get("mysqlPassword");
-        String mysqlUser = map.get("mysqlUser");
-        String redisPassword = map.get("redisPassword");
-        String redisHost    = map.get("redisHost");
-        String redisPort    = map.get("redisPort");
-
-
         MySqlSource<String> mysql = MySqlSource.<String>builder()
-                .hostname(mysqlHost)
-                .port(mysqlPort)
+                .hostname("10.150.20.11")
+                .port(3306)
                 .databaseList("member_db") // set captured database
                 .tableList("member_db.m_member_tenant") // set captured table
                 .username("readonly")
@@ -59,11 +38,11 @@ public class ConnectRedis {
                 .startupOptions(StartupOptions.initial())
                 .build();
 
-        //读取数据源
+
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment().setParallelism(1);
+
         DataStreamSource<String> aa = env.fromSource(mysql, WatermarkStrategy.noWatermarks(), "aa");
-
-
-
 
         SingleOutputStreamOperator<JSONObject> JSONOBj = aa.map(new MapFunction<String, JSONObject>() {
             @Override
@@ -72,8 +51,12 @@ public class ConnectRedis {
             }
         });
 
+
+        JSONOBj.print();
+
+
         // 连接到Redis的配置
-        FlinkJedisPoolConfig redisConfig = new FlinkJedisPoolConfig.Builder()
+        FlinkJedisPoolConfig redisConfig = new  FlinkJedisPoolConfig.Builder()
                 .setHost("10.150.60.5")
                 .setPort(30079)
                 .setPassword("bigdata1234")
